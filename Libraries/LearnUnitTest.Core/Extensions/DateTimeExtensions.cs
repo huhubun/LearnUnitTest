60bece28d8a227ddb96ed208c08b48f691f176fe
huhubun <nongzc@outlook.com>
2017-01-18 02:12:42 +0800

    Create project

---
+++ Libraries/LearnUnitTest.Core/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LearnUnitTest.Core.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static bool In(this DateTime date, DateTime startDate, DateTime endDate)
+        {
+            if (endDate < startDate)
+            {
+                throw new ArgumentException($"{nameof(endDate)} must be greater than {nameof(startDate)}", nameof(endDate));
+            }
+
+            return startDate <= date && date <= endDate;
+        }
+    }
+}
---
+++ Tests/LearnUnitTest.Core.Test/Extensions/DateTimeExtensionsTest.cs
@@ -0,0 +1,53 @@
+﻿using LearnUnitTest.Core.Extensions;
+using System;
+using Xunit;
+
+namespace LearnUnitTest.Core.Test.Extensions
+{
+    public class DateTimeExtensionsTest
+    {
+        [Fact]
+        public void Date_in_two_dates()
+        {
+            // Arrange
+            var date = new DateTime(2017, 1, 27);
+            var startDate = new DateTime(2017, 1, 23);
+            var endDate = new DateTime(2017, 1, 29);
+
+            // Act
+            var isIn = date.In(startDate, endDate);
+
+            // Assert
+            Assert.Equal(true, isIn);
+        }
+
+        [Fact]
+        public void Date_not_in_two_dates()
+        {
+            // Arrange
+            var date = new DateTime(2017, 1, 16);
+            var startDate = new DateTime(2017, 1, 23);
+            var endDate = new DateTime(2017, 1, 29);
+
+            // Act
+            var isIn = date.In(startDate, endDate);
+
+            // Assert
+            Assert.Equal(false, isIn);
+        }
+
+        [Fact]
+        public void EndDate_is_less_than_startDate()
+        {
+            // Arrange
+            var date = new DateTime(2017, 1, 27);
+            var startDate = new DateTime(2017, 1, 23);
+            var endDate = new DateTime(2017, 1, 1);
+
+            // Act
+
+            // Assert
+            Assert.Throws<ArgumentException>("endDate", () => date.In(startDate, endDate));
+        }
+    }
+}
