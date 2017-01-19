d29bfcde5f841345cdd8c613444881ef8e86e338
huhubun <nongzc@outlook.com>
2017-01-19 15:39:47 +0800

    添加 AutoFixture.AutoMoq

--- Tests/LearnUnitTest.Services.Test/Users/UsernameExistsTest.cs
+++ Tests/LearnUnitTest.Services.Test/Users/UsernameExistsTest.cs
@@ -1,13 +1,92 @@
-﻿using System;
+﻿using LearnUnitTest.Core.Data;
+using LearnUnitTest.Core.Domain.Users;
+using LearnUnitTest.Services.Users;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xunit;
 
 namespace LearnUnitTest.Services.Test.Users
 {
     public class UsernameExistsTest
     {
+        private static List<User> _userDataSource = new List<User>
+        {
+            new User { Id = 1, Username ="admin", Password = "Pa$$w0rd" },
+            new User { Id = 2, Username ="supervisor", Password = "123456" },
+            new User { Id = 3, Username ="zhangsan", Password = "1111" }
+        };
+
+        private readonly IRepository<User> _userRepository;
+        private readonly UserService _userService;
+
+        public UsernameExistsTest()
+        {
+            var mockUserRepository = new Mock<IRepository<User>>();
+            mockUserRepository.SetupGet(r => r.Table).Returns(_userDataSource.AsQueryable());
+
+            _userRepository = mockUserRepository.Object;
+            _userService = new UserService(_userRepository);
+        }
+
+        [Fact]
+        public void When_username_is_null_returns_false()
+        {
+            // Arrange
+            string username = null;
+
+            // Act
+            var isExists = _userService.IsUsernameExists(username);
+
+            // Assert
+            Assert.False(isExists);
+        }
+
+        [Fact]
+        public void When_username_is_empty_returns_false()
+        {
+            // Arrange
+            var username = String.Empty;
+
+            // Act
+            var isExists = _userService.IsUsernameExists(username);
+
+            // Assert
+            Assert.False(isExists);
+        }
+
+        [Theory]
+        [InlineData("admin")]
+        [InlineData("supervisor")]
+        [InlineData("zhangsan")]
+        public void When_username_is_exists_returns_true(string username)
+        {
+            // Arrange
+
+
+            // Act
+            var isExists = _userService.IsUsernameExists(username);
+
+            // Assert
+            Assert.True(isExists);
+        }
+
+        [Theory]
+        [InlineData("admin ")]
+        [InlineData(" admin")]
+        [InlineData("lisi")]
+        public void When_username_is_null_returns_false(string username)
+        {
+            // Arrange
+
+
+            // Act
+            var isExist = _userService.IsUsernameExists(username);
+
+            // Assert
+            Assert.False(isExist);
+        }
 
     }
 }
