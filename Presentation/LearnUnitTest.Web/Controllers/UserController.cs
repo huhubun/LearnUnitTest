dd1640922d131aae71ea4853381a7a17ffc3918a
huhubun <nongzc@outlook.com>
2017-02-09 00:05:53 +0800

    Using AutoFac via NuGet and add UserController

---
+++ Presentation/LearnUnitTest.Web/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using LearnUnitTest.Services.Users;
+using LearnUnitTest.Web.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnUnitTest.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserModel model)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Presentation/LearnUnitTest.Web/Infrastructure/DependencyRegistrar.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using LearnUnitTest.Services.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnUnitTest.Web.Infrastructure
+{
+    public static class DependencyRegistrar
+    {
+        public static void Register()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            //
+            builder.RegisterType<UserService>().InstancePerLifetimeScope();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
